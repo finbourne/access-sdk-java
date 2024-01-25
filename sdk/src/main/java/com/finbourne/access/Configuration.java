bee56a410277dfc8ae179656ab01b879583f5097
Concourse <concourse@finbourne.com>
2024-01-25 10:26:28 +0000

    Updates for v2.0.46

--- sdk/src/main/java/com/finbourne/access/Configuration.java
+++ sdk/src/main/java/com/finbourne/access/Configuration.java
@@ -12,7 +12,7 @@ package com.finbourne.access;
 
 @jakarta.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen")
 public class Configuration {
-    public static final String VERSION = "2.0.44";
+    public static final String VERSION = "2.0.46";
 
     private static ApiClient defaultApiClient = new ApiClient();
 
